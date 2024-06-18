68d3de73eb251e65b09176a7f8da0136fb198d02
vert-pjoubert <152121204+vert-pjoubert@users.noreply.github.com>
2024-06-18 18:17:09 +0000

    cleaning up some uses of temporary data.

--- render_test.go
+++ render_test.go
@@ -8,52 +8,76 @@ import (
 	"testing"
 
 	"github.com/a-h/templ"
+	"github.com/vert-pjoubert/goth-template/store/models"
 	"github.com/vert-pjoubert/goth-template/templates"
 )
 
 // Test full page render with layout
 func TestRenderWithLayout(t *testing.T) {
-    // Set up renderer
-    renderer := NewTemplRenderer()
+	// Set up renderer
+	renderer := NewTemplRenderer()
+	appStore := &mockAppStore{}
 
-    testCases := []struct {
-        name      string
-        component templ.Component
-    }{
-        {"HomePage", templates.Home()},
-        {"SettingsPage", templates.Settings()},
-        {"ServersPage", templates.ServersList(Servers)},
-        {"EventsPage", templates.EventsList(Events)},
-    }
+	// Get data from mockAppStore
+	var servers []models.Server
+	var events []models.Event
+	err := appStore.GetServers(&servers)
+	if err != nil {
+		t.Fatalf("Failed to get servers: %v", err)
+	}
+	err = appStore.GetEvents(&events)
+	if err != nil {
+		t.Fatalf("Failed to get events: %v", err)
+	}
 
-    dumpDir := "./test/dump"
-    err := os.MkdirAll(dumpDir, 0755)
-    if err != nil {
-        t.Fatalf("Failed to create dump directory: %v", err)
-    }
+	templateServers := make([]templates.Server, len(servers))
+	for i, server := range servers {
+		templateServers[i] = templates.NewServer(server)
+	}
 
-    for _, tc := range testCases {
-        t.Run(tc.name, func(t *testing.T) {
-            req := httptest.NewRequest("GET", "/", nil)
-            w := httptest.NewRecorder()
+	templateEvents := make([]templates.Event, len(events))
+	for i, event := range events {
+		templateEvents[i] = templates.NewEvent(event)
+	}
 
-            renderer.RenderWithLayout(w, tc.component, req)
+	testCases := []struct {
+		name      string
+		component templ.Component
+	}{
+		{"HomePage", templates.Home()},
+		{"SettingsPage", templates.Settings()},
+		{"ServersPage", templates.ServersList(templateServers)},
+		{"EventsPage", templates.EventsList(templateEvents)},
+	}
 
-            res := w.Result()
-            defer res.Body.Close()
+	dumpDir := "./test/dump"
+	err = os.MkdirAll(dumpDir, 0755)
+	if err != nil {
+		t.Fatalf("Failed to create dump directory: %v", err)
+	}
 
-            output, err := io.ReadAll(res.Body)
-            if err != nil {
-                t.Fatalf("Failed to read response body for %s: %v", tc.name, err)
-            }
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			req := httptest.NewRequest("GET", "/", nil)
+			w := httptest.NewRecorder()
 
-            dumpPath := filepath.Join(dumpDir, tc.name+".html")
-            err = os.WriteFile(dumpPath, output, 0644)
-            if err != nil {
-                t.Fatalf("Failed to write dump file for %s: %v", tc.name, err)
-            }
+			renderer.RenderWithLayout(w, tc.component, req)
 
-            t.Logf("Dumped %s to %s", tc.name, dumpPath)
-        })
-    }
+			res := w.Result()
+			defer res.Body.Close()
+
+			output, err := io.ReadAll(res.Body)
+			if err != nil {
+				t.Fatalf("Failed to read response body for %s: %v", tc.name, err)
+			}
+
+			dumpPath := filepath.Join(dumpDir, tc.name+".html")
+			err = os.WriteFile(dumpPath, output, 0644)
+			if err != nil {
+				t.Fatalf("Failed to write dump file for %s: %v", tc.name, err)
+			}
+
+			t.Logf("Dumped %s to %s", tc.name, dumpPath)
+		})
+	}
 }
